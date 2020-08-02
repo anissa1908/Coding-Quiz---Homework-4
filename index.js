88ffd2afefef3d8d99de04222044e0a99045a781
Anissa Shanks <anissa.shanks@gmail.com>
2020-08-02 14:24:22 -0500

    added functions on javascript file to build the quiz show results and added onclick event to show resutls

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var startButton = $('.start').startButton
 var totalQuestions = $('.questions').size(8);
 
 //Set the current question to display to 1
-var currentQuestion = 1;
+var currentQuestion = 0;
 
 //Store the selector in a variable.
 $questions = $('.questions');
@@ -34,7 +34,7 @@ $('#next').click(function () {
         //increment the current question by one
         currentQuestion = currentQuestion + 1;
 
-        //if there are no more questions do stuff
+        //if there are no further questions to answer
         if (currentQuestion == totalQuestions) {
 
             var result = sum_values()
@@ -52,15 +52,32 @@ $('#next').click(function () {
 
 });
 
+// store references to variables
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+// functions 
+
+function buildQuiz() {}
+function showResults() {}
+
+// show quiz immediately
+buildQuiz();
+
+// when quiz is finished, show results
+submitButton.onclick('click', showResults)
+
 // used object literals to represent each question in the quiz and array to hold all of the questions
-const myQuestions = [
+const myQuestions = []
     {
         question: "What year did Kobe get drafted to the NBA?",
         A. 1998
         B. 1991
         C. 1996
         D. 1994
-    },
+    };
   
 
 {
@@ -69,7 +86,7 @@ const myQuestions = [
         B. Charlotte Hornets
         C. Chicago Bulls
         D. Portland Trailblazers
-}
+};
 
 {
         question: "What nickname is Kobe know by?"
@@ -77,7 +94,7 @@ const myQuestions = [
         B. KB 24
         C. Black Mamba
         D. All Of The Above
-}
+};
 
 {
         question: "How many points did Kobe score in his highest scoring game?"
@@ -85,7 +102,7 @@ const myQuestions = [
         B. 81
         C. 60
         D. 48
-}
+};
 
 {
         question: "What film did Kobew win an Academy Award for?"
@@ -93,7 +110,7 @@ const myQuestions = [
         B. The Last Dance
         C. Dear BAsketball
         D. The Wizinard Series
-}
+};
 
 {
         question: "What model did Kobe collaborate with on a rap single?"
@@ -101,7 +118,7 @@ const myQuestions = [
         B. Queen Latifah
         C. Naomi Campbell
         D. Veronica Webb
-}
+};
 
 {
         question: "How many NBA Championship rings did Kobe win in his career?"
@@ -109,7 +126,7 @@ const myQuestions = [
         B. Four
         C. Seven
         D. Six
-}
+};
 
 {
         question: "How many siblings does Kobe have?"
@@ -117,7 +134,7 @@ const myQuestions = [
         B. Four
         C. Two
         D. Zero
-}
+};
 
 {
         question: "What NBA player lived with Kobe and his family?"
@@ -125,7 +142,9 @@ const myQuestions = [
         B. Penny Hardaway
         C. Allen Iverson
         D. Tracy McGrady
-}
+
+};
+
 
 
 
